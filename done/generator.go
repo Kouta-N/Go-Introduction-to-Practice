3810f5303df07437f083499a9dee1b325fb3fe3b
KN <nk29time@gmail.com>
2024-01-08 08:30:06 +0900

    impl test

--- done/generator.go
+++ done/generator.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
-func main() {
-	quit := make(chan string)
-	ch := generator("Hi", quit)
-	for i := rand.Intn(9); i >= 0; i-- {
-		fmt.Println(<-ch, i)
-	}
-	quit <- "Bye!"
-	fmt.Printf("Generator says %s", <-quit)
-}
+// func main() {
+// 	quit := make(chan string)
+// 	ch := generator("Hi", quit)
+// 	for i := rand.Intn(9); i >= 0; i-- {
+// 		fmt.Println(<-ch, i)
+// 	}
+// 	quit <- "Bye!"
+// 	fmt.Printf("Generator says %s", <-quit)
+// }
 
 func generator(msg string, quit chan string) <-chan string {
 	ch := make(chan string)
